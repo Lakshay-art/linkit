047ebd0e1e6341c78198627a2a10f9f9400066f1
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2022-04-27 14:20:23 +0530

    optimized

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://linkiie-git-master-lakshay-art.vercel.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://linkiie-git-master-lakshay-art.vercel.app'

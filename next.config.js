f3f890f99007ed675b01ccfbfa51f549a96d67b3
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2022-04-28 01:32:53 +0530

    optimized

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    webpack: (config, { dev }) => {
-      // Perform customizations to config
-      config.node = {
-        fs: 'empty'
-      };
-      return config
-    }
-  }

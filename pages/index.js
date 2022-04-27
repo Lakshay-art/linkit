7c722209c10e75603e51f4bceb10d18b35155422
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2022-04-27 14:27:55 +0530

    optimized

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,15 @@ export default function Home({ articles }) {
   )
 }
 
-export const getServerSideProps = async () => {
-  const res = await fetch(`api/articles`)
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return {
     props: {
       articles,
     },
-      //  revalidate:10
+        revalidate:10
   }
 
   

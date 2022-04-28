e1a054c1ac09d85789ce718f31c9eb714c857eaf
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2022-04-29 01:15:37 +0530

    optimized

--- components/Meta.js
+++ components/Meta.js
@@ -7,16 +7,17 @@ const Meta = ({ title, keywords, description }) => {
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
       <meta charSet='utf-8' />
-      <link rel='icon' href='/favicon.ico' />
+      {/* <link rel='icon' href='/favicon.ico' /> */}
+      <link rel="icon" href="/icon2.png"></link>
       <title>{title}</title>
     </Head>
   )
 }
 
 Meta.defaultProps = {
-  title: 'WebDev Newz',
-  keywords: 'web development, programming',
-  description: 'Get the latest news in web dev',
+  title: 'Linkerr',
+  keywords: 'websites, share ,links',
+  description: 'Share your public links ,actually to the world',
 }
 
 export default Meta
